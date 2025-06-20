40aeb9c183602b141bd12fbbeb6b4d41b44f4d48
Alvin Mugoya <mugoyaalvin018@gmail.com>
2025-06-20 11:37:45 +0300

    Alvin's commit

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,7 @@
-def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
